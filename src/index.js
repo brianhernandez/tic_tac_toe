2a1a1df086998f1ae248ab53070b5809bd25c729
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-07-24 17:50:55 -0400

    Add x, o input functions, add reset function.  Add styling of game board

--- src/index.js
+++ src/index.js
@@ -5,5 +5,61 @@ window.$ = $;
 window.jQuery = jQuery;
 
 $(document).ready(function() {
-  $('body').append('<div class="hello">HELLO Moto</div>');
+  // $('body').append('<div class="hello">HELLO Moto</div>');
+  var userSymbol = "X";
+  var computerSymbol = "0";
+  var tictactoeArray = ['*','*','*','*','*','*','*','*','*',];
+  var gameState = false;
+  var count = 0;
+
+  $('.container').click(function(event) {
+    event.preventDefault();
+    if ($(event.target).is('#x-button')) {
+      userSymbol = "X";
+      computerSymbol = "O";
+      $('#x-button').addClass('btn-primary');
+      $('#o-button').removeClass('btn-primary');
+      resetGame();
+    } else if ($(event.target).is('#o-button')) {
+      userSymbol = "O";
+      computerSymbol = "X";
+      $('#o-button').addClass('btn-primary');
+      $('#x-button').removeClass('btn-primary');
+      resetGame();
+    }
+
+    if ($(event.target).is('.reset')) {
+      resetGame();
+    }
+
+    if ($(event.target).is('.tic')) {
+      var slot = $(event.target).attr('id');
+      // console.log($(event.target).attr('id'));
+      userTurn(userSymbol, slot);
+    }
+
+    function userTurn(symbol, id) {
+      var slotTaken = $('#' + id).text();
+      // console.log(slotTaken);
+      if (slotTaken === '*') {
+        count++;
+        tictactoeArray[id - 1] = symbol;
+        $('#' + id).text(symbol);
+        //doWeHaveAWinner(tictactoeArray, symbol)
+        console.log(tictactoeArray);
+      }
+    }
+
+    function doWeHaveAWinner(tictactoeArray, symbol){
+
+    }
+
+  });
+
+  function resetGame() {
+    tictactoeArray = ['*','*','*','*','*','*','*','*','*',];
+    count = 0;
+    $('.tic').text('*');
+    gameState = true;
+  }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+    watchContentBase: true
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
