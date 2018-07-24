7553f40282c71a83fa644a9ba9b2f5c0dcec1d37
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-07-24 18:41:03 -0400

    Add playerTurn and doWeHaveAWinner functions.  Add winning tile styles

--- src/index.js
+++ src/index.js
@@ -33,33 +33,80 @@ $(document).ready(function() {
     }
 
     if ($(event.target).is('.tic')) {
-      var slot = $(event.target).attr('id');
+      var gameBoardSlot = $(event.target).attr('id');
       // console.log($(event.target).attr('id'));
-      userTurn(userSymbol, slot);
+      playerTurn(userSymbol, gameBoardSlot);
     }
 
-    function userTurn(symbol, id) {
-      var slotTaken = $('#' + id).text();
-      // console.log(slotTaken);
-      if (slotTaken === '*') {
+    function playerTurn(symbol, id) {
+      var slotValue = $('#' + id).text();
+      // console.log(slotValue);
+      if (slotValue === '*') {
         count++;
         tictactoeArray[id - 1] = symbol;
         $('#' + id).text(symbol);
-        //doWeHaveAWinner(tictactoeArray, symbol)
+        doWeHaveAWinner(tictactoeArray, symbol)
         console.log(tictactoeArray);
       }
     }
 
-    function doWeHaveAWinner(tictactoeArray, symbol){
-
+    function doWeHaveAWinner(tictactoeArray, currentTurn) {
+      if (tictactoeArray[0] === currentTurn && tictactoeArray[1]===currentTurn && tictactoeArray[2] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#1, #2, #3').addClass('winning-tile');
+      } else if (tictactoeArray[2] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[6] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#3, #5, #7').addClass('winning-tile');
+      } else if (tictactoeArray[0] === currentTurn && tictactoeArray[3]===currentTurn && tictactoeArray[6] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#1, #4, #7').addClass('winning-tile');
+      } else if (tictactoeArray[0] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[8] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#1, #5, #9').addClass('winning-tile');
+      } else if (tictactoeArray[1] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[7] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#2, #5, #8').addClass('winning-tile');
+      } else if (tictactoeArray[2] === currentTurn && tictactoeArray[5]===currentTurn && tictactoeArray[8] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#3, #6, #9').addClass('winning-tile');
+      } else if (tictactoeArray[2] === currentTurn && tictactoeArray[5]===currentTurn && tictactoeArray[8] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#3, #6, #9').addClass('winning-tile');
+      } else if (tictactoeArray[3] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[5] === currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#4, #5, #6').addClass('winning-tile');
+      } else if (tictactoeArray[6]===currentTurn && tictactoeArray[7]===currentTurn && tictactoeArray[8]===currentTurn) {
+        gameState = true;
+        // resetGame();
+        // console.log('winner winner chicken dinner');
+        $('#7, #8, #9').addClass('winning-tile');
+      } else {
+        gameState = false;
+      }
     }
-
   });
 
   function resetGame() {
     tictactoeArray = ['*','*','*','*','*','*','*','*','*',];
     count = 0;
     $('.tic').text('*');
+    $('.winning-tile').removeClass('winning-tile');
     gameState = true;
   }
 })
