61b1485e0ff2b9567476d846f7bbe98c831bb4eb
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-07-24 21:40:43 -0400

    Add computerTurn function.  Add doWeHaveAWinner function that checks win

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ window.$ = $;
 window.jQuery = jQuery;
 
 $(document).ready(function() {
-  // $('body').append('<div class="hello">HELLO Moto</div>');
   var userSymbol = "X";
   var computerSymbol = "0";
   var tictactoeArray = ['*','*','*','*','*','*','*','*','*',];
@@ -26,87 +25,104 @@ $(document).ready(function() {
       $('#o-button').addClass('btn-primary');
       $('#x-button').removeClass('btn-primary');
       resetGame();
-    }
-
-    if ($(event.target).is('.reset')) {
+    } else if ($(event.target).is('.reset')) {
       resetGame();
     }
 
     if ($(event.target).is('.tic')) {
       var gameBoardSlot = $(event.target).attr('id');
-      // console.log($(event.target).attr('id'));
       playerTurn(userSymbol, gameBoardSlot);
     }
 
     function playerTurn(symbol, id) {
       var slotValue = $('#' + id).text();
-      // console.log(slotValue);
+      
       if (slotValue === '*') {
         count++;
         tictactoeArray[id - 1] = symbol;
         $('#' + id).text(symbol);
-        doWeHaveAWinner(tictactoeArray, symbol)
-        console.log(tictactoeArray);
+        doWeHaveAWinner(tictactoeArray, symbol);
+
+        if (gameState === false) {
+          computerTurn(computerSymbol);
+          doWeHaveAWinner(tictactoeArray, computerSymbol);
+          console.log(tictactoeArray);
+        }
       }
     }
 
-    function doWeHaveAWinner(tictactoeArray, currentTurn) {
-      if (tictactoeArray[0] === currentTurn && tictactoeArray[1]===currentTurn && tictactoeArray[2] === currentTurn) {
+    function computerTurn(symbol) {
+      var slotTaken = false;
+
+      while(slotTaken === false && count < 6) {
+        var computersChoiceSlot = (Math.random() * 10).toFixed();
+        var moveSlotText = $('#' + computersChoiceSlot).text();
+
+        if (moveSlotText === '*') {
+          slotTaken = true;
+          $('#' + computersChoiceSlot).text(symbol);
+          tictactoeArray[computersChoiceSlot - 1] = symbol;
+        }
+      }
+    }
+
+    function doWeHaveAWinner(tictactoeArray, currentTurnSymbol) {
+      if (tictactoeArray[0] === currentTurnSymbol
+        && tictactoeArray[1] === currentTurnSymbol
+        && tictactoeArray[2] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#1, #2, #3').addClass('winning-tile');
-      } else if (tictactoeArray[2] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[6] === currentTurn) {
+      } else if (tictactoeArray[2] === currentTurnSymbol
+        && tictactoeArray[4] === currentTurnSymbol
+        && tictactoeArray[6] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#3, #5, #7').addClass('winning-tile');
-      } else if (tictactoeArray[0] === currentTurn && tictactoeArray[3]===currentTurn && tictactoeArray[6] === currentTurn) {
+      } else if (tictactoeArray[0] === currentTurnSymbol
+        && tictactoeArray[3] === currentTurnSymbol
+        && tictactoeArray[6] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#1, #4, #7').addClass('winning-tile');
-      } else if (tictactoeArray[0] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[8] === currentTurn) {
+      } else if (tictactoeArray[0] === currentTurnSymbol
+        && tictactoeArray[4] === currentTurnSymbol
+        && tictactoeArray[8] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#1, #5, #9').addClass('winning-tile');
-      } else if (tictactoeArray[1] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[7] === currentTurn) {
+      } else if (tictactoeArray[1] === currentTurnSymbol
+        && tictactoeArray[4] === currentTurnSymbol
+        && tictactoeArray[7] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#2, #5, #8').addClass('winning-tile');
-      } else if (tictactoeArray[2] === currentTurn && tictactoeArray[5]===currentTurn && tictactoeArray[8] === currentTurn) {
+      } else if (tictactoeArray[2] === currentTurnSymbol
+        && tictactoeArray[5] === currentTurnSymbol
+        && tictactoeArray[8] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#3, #6, #9').addClass('winning-tile');
-      } else if (tictactoeArray[2] === currentTurn && tictactoeArray[5]===currentTurn && tictactoeArray[8] === currentTurn) {
+      } else if (tictactoeArray[2] === currentTurnSymbol
+        && tictactoeArray[5] === currentTurnSymbol
+        && tictactoeArray[8] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#3, #6, #9').addClass('winning-tile');
-      } else if (tictactoeArray[3] === currentTurn && tictactoeArray[4]===currentTurn && tictactoeArray[5] === currentTurn) {
+      } else if (tictactoeArray[3] === currentTurnSymbol
+        && tictactoeArray[4] === currentTurnSymbol
+        && tictactoeArray[5] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#4, #5, #6').addClass('winning-tile');
-      } else if (tictactoeArray[6]===currentTurn && tictactoeArray[7]===currentTurn && tictactoeArray[8]===currentTurn) {
+      } else if (tictactoeArray[6]===currentTurnSymbol
+        && tictactoeArray[7] === currentTurnSymbol
+        && tictactoeArray[8] === currentTurnSymbol) {
         gameState = true;
-        // resetGame();
-        // console.log('winner winner chicken dinner');
         $('#7, #8, #9').addClass('winning-tile');
       } else {
         gameState = false;
       }
     }
-  });
 
-  function resetGame() {
-    tictactoeArray = ['*','*','*','*','*','*','*','*','*',];
-    count = 0;
-    $('.tic').text('*');
-    $('.winning-tile').removeClass('winning-tile');
-    gameState = true;
-  }
-})
+    function resetGame() {
+      tictactoeArray = ['*','*','*','*','*','*','*','*','*',];
+      count = 0;
+      $('.tic').text('*');
+      $('.winning-tile').removeClass('winning-tile');
+      gameState = true;
+    }
+  });
+});
